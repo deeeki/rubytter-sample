1abf5ca2360f16615e569aa7fc21eec96730aa42
deeeki <deeeki+gh@gmail.com>
2011-11-25 02:34:19 +0900

    use "cursor" param

--- unfollow_adjustment.rb
+++ unfollow_adjustment.rb
@@ -1,13 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
 @rubytter = OAuthRubytter.new(@access_token)
-friends_ids = @rubytter.friends_ids nil
-followers_ids = @rubytter.followers_ids nil
+friends_ids = @rubytter.friends_ids(nil, :cursor => -1).ids
+followers_ids = @rubytter.followers_ids(nil, :cursor => -1).ids
 followings_ids = friends_ids - followers_ids
 p followings_ids
-followings_ids.each do |i|
-	user = @rubytter.user(i)
-	p user.screen_name
-	p user.description
-	p @rubytter.leave(i)
+followings_ids.each do |id|
+  user = @rubytter.user(id)
+  @rubytter.leave(id)
+  puts user.screen_name, user.description
+  puts
 end
