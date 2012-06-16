a9101d5eda6b805842a805a6d18d1152615f9794
deeeki <deeeki+gh@gmail.com>
2012-06-16 11:21:14 +0900

    modify to clear lists

--- clear_list.rb
+++ clear_list.rb
@@ -1,11 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 @rubytter = OAuthRubytter.new(@access_token)
+owner_name = 'deeeki'
+list_name = 'geek'
+
 nc = -1
 begin
-	lm = @rubytter.list_members('deeeki', 'watching', :cursor => nc)
-	nc = lm.next_cursor
-	lm.users.each do |u|
-		p @rubytter.remove_member_from_list('deeeki', 'watching', u.id)
-	end
+  lm = @rubytter.list_members(owner_name, list_name, :cursor => nc)
+  nc = lm.next_cursor
+  lm.users.each do |u|
+    p @rubytter.post('/lists/members/destroy', {:slug => list_name, :owner_screen_name => owner_name, :user_id => u.id})
+  end
 end until nc.zero?
